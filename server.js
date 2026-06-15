56b10b09ed29093b5ed2e22c9548ef972e5046d8
AGullifer444 <aligullifer@gmail.com>
2026-06-14 21:58:08 -0700

    added node server. js and modified events and add-events

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 app.post("/add-event", function(req, res) {
     console.log(req.body);
     res.redirect("/events.html");
 });
+
+
 
 app.listen(PORT, function() {
     console.log(`Server running at http://localhost:${PORT}`);
