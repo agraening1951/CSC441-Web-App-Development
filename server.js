a38daa19475c57db1e443f67a63d70898d77d118
agraening1951 <a.graening1951@student.nu.edu>
2026-06-17 10:38:54 -0700

    added express routes and fixed styling on calendar

--- server.js
+++ server.js
@@ -2,19 +2,39 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/events", function(req, res) {
+    res.sendFile(path.join(__dirname, "events.html"));
+});
+
+app.get("/calendar", function(req, res) {
+    res.sendFile(path.join(__dirname, "calendar.html"));
+});
+
+app.get("/add-event", function(req, res) {
+    res.sendFile(path.join(__dirname, "add-event.html"));
+});
+
+app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "about.html"));
+});
 
 app.post("/add-event", function(req, res) {
     console.log(req.body);
     res.redirect("/events.html");
 });
 
-
-
 app.listen(PORT, function() {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
+
